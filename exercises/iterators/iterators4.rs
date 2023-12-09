2b6e0cedbe7d84cc96de2bdb60ca3af6219adbed
Andrew Clough <andrew.clough@gmail.com>
2023-12-09 16:53:42 -0500

    Done through 80, Arc

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,9 +3,11 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    match (1..=num).reduce(|a, b| a * b) {
+        None => 1,
+        Some(n) => n,
+    }
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
